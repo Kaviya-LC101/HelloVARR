0e9a8c27617c94e13762b91b941df251646e0c8e
kaviya <kaviya0925@gmail.com>
2021-12-12 13:25:54 -0600

    Add  SpringBoot Application

---
+++ src/main/java/org/example/contollers/HelloController.java
@@ -0,0 +1,13 @@
+package org.example.contollers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @GetMapping("/")
+    public String sayHello() {
+        return "index";
+    }
+}
+
